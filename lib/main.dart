0522a713e3cd613595cc01117c054024a3efcb46
Abhijeet S Sonawane <abhijeet126007@gmail.com>
2024-01-16 19:05:18 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:clean_architecture_news_app_demo/config/theme/app_themes.dart';
 import 'package:clean_architecture_news_app_demo/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:clean_architecture_news_app_demo/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
@@ -5,6 +7,7 @@ import 'package:clean_architecture_news_app_demo/features/daily_news/presentatio
 import 'package:clean_architecture_news_app_demo/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,8 +31,120 @@ class MyApp extends StatelessWidget {
         //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         //   useMaterial3: true,
         // ),
-        home: const DailyNews(),
+        home: ConnectivityRecheck(),
       ),
     );
   }
 }
+
+class ConnectivityRecheck extends StatelessWidget {
+  const ConnectivityRecheck({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: MaterialButton(
+                  color: Colors.greenAccent,
+                    child: Text("Check for internet connection.."),
+                    onPressed: () {}),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//
+// class Demo extends StatefulWidget {
+//   const Demo({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Demo> createState() => _DemoState();
+// }
+//
+// class _DemoState extends State<Demo> {
+//   final _dataStreamController = StreamController<String>.broadcast();
+//
+//   Stream<String> get dataStream$ => _dataStreamController.stream;
+//
+//   addDataToStream(String data) => _dataStreamController.sink.add(data);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Builder(builder: (context) {
+//                 return MaterialButton(
+//                     color: Colors.redAccent,
+//                     onPressed: () => _callApi(context),
+//                     child: const Text("Tap"));
+//               }),
+//             ],
+//           )
+//         ],
+//       ),
+//     );
+//   }
+//
+//   void _callApi(context) {
+//     /// show bottomsheet
+//     bloc();
+//
+//     Scaffold.of(context).showBottomSheet<void>((BuildContext context) {
+//       return Material(
+//         color: Colors.greenAccent,
+//         child: SizedBox(
+//           height: 200,
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               StreamBuilder(
+//                   stream: dataStream$,
+//                   builder:
+//                       (BuildContext context, AsyncSnapshot<String> snapshot) {
+//                     if (snapshot.hasData) {
+//                       return Column(
+//                         mainAxisAlignment: MainAxisAlignment.center,
+//                         children: [
+//                           Row(
+//                             mainAxisAlignment: MainAxisAlignment.center,
+//                             children: [
+//                               Text(snapshot.data.toString()),
+//                             ],
+//                           ),
+//                         ],
+//                       );
+//                     }
+//                     if (snapshot.hasError) {
+//                       return Text(snapshot.error.toString());
+//                     }
+//                     return const Center(child: CircularProgressIndicator());
+//                   }),
+//             ],
+//           ),
+//         ),
+//       );
+//     });
+//   }
+//
+//   bloc() {
+//     Future.delayed(const Duration(milliseconds: 1200), () {
+//       var rng = Random();
+//       addDataToStream(rng.nextInt(100).toString());
+//     });
+//   }
+// }
